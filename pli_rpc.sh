7c0979d16bce5e38d1bc71a3cbb00595ff463d8f
inv4fee2020 <inv4fee2020@gmail.com>
2022-08-21 14:01:54 +0100

    file check & pm2 restart added

--- pli_rpc.sh
+++ pli_rpc.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-#The new RPC:  update 2_nodeStartPM2.sh
-#https://pli.xdcrpc.com
 
-#The new WS:  update 3_initiatorStartPM2.sh
-#wss://pli.xdcrpc.com/ws
+    # Set bashfile2
+    if [ -e ~/plugin-deployment/2_nodeStartPM2.sh ]; then
+        BASH_FILE2="2_nodeStartPM2.sh"
+    elif [ -e ~/plugin-deployment/[sS]tart[nN]ode.sh ]; then
+        BASH_FILE2="[sS]tart[nN]ode.sh"
+    fi
+    echo $BASH_FILE2
 
-BASH_FILE2="2_nodeStartPM2.sh"
-BASH_FILE3="3_initiatorStartPM2.sh"
 
-#    sed -i 's/^API_PASS.*/API_PASS='"${_AUTOGEN_API_PWD}"'/g' ~/"plinode_$(hostname -f)".vars
+    # Set bashfile3
+    if [ -e ~/plugin-deployment/3_initiatorStartPM2.sh ]; then
+        BASH_FILE3="3_initiatorStartPM2.sh"
+    elif [ -e ~/plugin-deployment/[sS]tart[eE][iI].sh ]; then
+        BASH_FILE3="[sS]tart[eE][iI].sh"
+    fi
+    echo $BASH_FILE3
 
 sed -i 's|^export ETH_URL.*|export ETH_URL=wss://pli.xdcrpc.com/ws|g' ~/plugin-deployment/$BASH_FILE2
 cat $BASH_FILE2 | grep ETH_URL
 
 sed -i 's|plirpc.blocksscan.io|pli.xdcrpc.com|g' ~/plugin-deployment/$BASH_FILE3
 cat $BASH_FILE3 | grep https
+
+pm2 restart all
+pm2 reset all
+pm2 list
